6b31775dc283948537e055bd2a70919443171d31
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-10 11:22:58 +0200

    refactoring

--- start/frontend/script.js
+++ start/frontend/script.js
@@ -3,29 +3,36 @@ function loadEvent() {
     getMaxValue: function (input) {
       let maxValues = input.split(",");
       let maxValue = 0;
-      for (let i = 0; i < maxValues.length; i++) {
+      /* for (let i = 0; i < maxValues.length; i++) {
         let tempValue = parseInt(maxValues[i]);
         if (tempValue > maxValue) {
           maxValue = tempValue;
         }
-      }
+      } */
+      maxValue = maxValues.reduce((previous, current) => {
+        let tempValue = parseInt(current);
+        return tempValue > previous ? tempValue : previous;
+      }, 0);
       return [maxValue];
     },
 
     getGreaterThan: function (input) {
       let greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
       let greaterNums = [];
-      for (let j = 0; j < greaterValues.length; j++) {
+      /* for (let j = 0; j < greaterValues.length; j++) {
         if (greaterValues[j] > parseInt(input)) {
           greaterNums.push(greaterValues[j]);
         }
-      }
+      } */
+      greaterNums = greaterValues.filter((actualValue) => {
+        return actualValue > parseInt(input);
+      });
       return greaterNums;
     },
 
     fizzBuzz: function (input) {
       let output = [];
-      for (let k = 1; k <= parseInt(input); k++) {
+      /* for (let k = 1; k <= parseInt(input); k++) {
         let value;
         if (k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
         else if (k % 3 === 0) value = "Fizz";
@@ -33,7 +40,20 @@ function loadEvent() {
         else value = k;
         output.push(value);
       }
-      return output;
+      return output; */
+      for (let value = 1; value <= parseInt(input); value++) {
+        output.push(value);
+      }
+      let fizziBuzzi = output.map((value) => {
+        if (value % 5 === 0 && value % 3 === 0) {
+          return "FizzBuzz";
+        } else if (value % 3 === 0) {
+          return "Fizz";
+        } else if (value % 5 === 0) {
+          return "Buzz";
+        } else return value;
+      });
+      return fizziBuzzi;
     },
   };
 
@@ -65,12 +85,14 @@ function loadEvent() {
   });
 
   function getOutput(output) {
-    let returnValue = ["The function starts"];
+    /* let returnValue = ["The function starts"];
     for (let i = 0; i < output.length; i++) {
       returnValue.push(output[i]);
     }
     returnValue.push("The function ends");
-    return returnValue;
+    return returnValue; */
+
+    return ["The function starts", ...output, "The function ends"];
   }
 
   function logResult(place, values) {
